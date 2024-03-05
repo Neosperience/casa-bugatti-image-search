e6a7566e8de97c64df82c5384035083662ee4b97
Luca Contini <luca.contini@neosperience.com>
2024-03-05 17:42:51 +0100

    updated utils

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -4,4 +4,6 @@ import logging
 import streamlit as st
 
 def load_env() -> None:
+    os.environ["AWS_ACCESS_KEY"] = st.secrets["AWS_ACCESS_KEY"]
+    os.environ["AWS_SECRET_ACCESS_KEY"] = st.secrets["AWS_SECRET_ACCESS_KEY"]
     logging.basicConfig(level=logging.INFO)
